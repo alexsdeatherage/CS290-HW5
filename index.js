103c9d7121f95bbbdec36284dde442a56960910c
Alex Deatherage <alexdeatherage4829@gmail.com>
2021-05-17 18:57:19 -0700

    add get request received

--- index.js
+++ index.js
@@ -12,19 +12,39 @@ app.use(express.urlencoded({ extended: false}));
 app.use(express.json())
 
 
+
+
+
 app.set('port', port);
 
 const getQueryParams = (req, res, next) => {
     res.locals.queryStuff = req.query;
+    res.locals.bodyStuff = req.body;
     next();
 }
 
 app.use(getQueryParams)
 
 app.get('/', (req, res) => {
-    console.log(res.locals.queryStuff)
-    res.render('home', res.locals.queryStuff);
+    // console.log(res.locals.queryStuff)
+    let qParams = [];
+    for (let i in res.locals.queryStuff) {
+        qParams.push({'name':i, 'value':res.locals.queryStuff[i]})
+    }
+    let context = {};
+    context.dataList = qParams;
+    console.log(context)
+    res.render('home', context);
+})
 
+app.post('/', (req, res) => {
+    let qParams = []
+    for (let i in res.locals.bodyStuff) {
+        qParams.push({'name':i, 'value':res.locals.bodyStuff[i]})
+    }
+    let context = {}
+    context.dataList = qParams;
+    res.render('home2', context)
 })
 
 app.use(function (req, res) {
